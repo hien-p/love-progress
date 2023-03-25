87d32bad8f6c33938df2588f36a328d91d824fe0
hien-p <hphan0390@gmail.com>
2023-03-25 14:12:55 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import BlueHeartIcon from './components/BlueHeartIcon';
+
 function Progress({ done }) {
   const [style, setStyle] = useState({});
   
@@ -15,7 +17,6 @@ function Progress({ done }) {
   }, [done]);
   
   return (
-    
     <div className="progress">
       <div className="progress-done" style={style}>
         {done}%
@@ -25,16 +26,50 @@ function Progress({ done }) {
 }
 
 function App() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+      const formattedDate = now.toLocaleString('en-GB', options);
+      setDate(formattedDate);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  
+
+  const handleButtonClick = () => {
+    setIsFormVisible(!isFormVisible);
+  }
+
   return (
-    <form className="shadow-form"> 
-    <>
-    <h1 > track the progress of our love</h1>
-      <p>Chloe's love Progress Bar</p>
-      <Progress done={45} />
-      <p>Harry's love Progress Bar</p>
-      <Progress done={70} />
-    </>
-    </form> 
+    <div>
+      <button className="heart-button" onClick={handleButtonClick}>
+        <BlueHeartIcon />
+        Press to see our love progress
+      </button>
+      {isFormVisible &&
+        <form className="shadow-form"> 
+          <>
+            <h1>Track the progress of our love</h1>
+            <p>Current time in Vietnam: {date.replace(',', '').replace(/\//g, '/')}</p>
+            {/* <div className={`cs ${isLogoRotated ? 'rotate' : ''}`}>
+              <img src="https://cdn.discordapp.com/avatars/953690599930859610/2d207a221f6c510f9250c427aa772222.webp?size=96" id="logo" alt="Logo" onClick={handleLogoClick} />
+            </div> */}
+            <p>Chloe's love Progress Bar</p>
+            <Progress done={45} />
+            {/* <div className={`cs ${isLogoRotated ? 'rotate' : ''}`}>
+              <img src="https://cdn.discordapp.com/avatars/725994929133715526/2b4eeff926a7ab0574e82be4f153ebeb.webp?size=96" id="logo" alt="Logo" />
+            </div> */}
+            <p>Harry's love Progress Bar</p>
+            <Progress done={70} />
+          </>
+        </form>
+      }
+    </div>
   );
 }
 
---
+++ src/components/BlueHeartIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BlueHeartIcon(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={props.width} height={props.height}>
+      <path fill="#2196f3" d="M12 20.4l-1.4-1.4C5.9 14.3 2 11.1 2 7.5 2 4.4 4.4 2 7.5 2c2 0 3.9.9 5.1 2.4C14.6 2.9 16.5 2 18.5 2 21.6 2 24 4.4 24 7.5c0 3.6-3.9 6.8-8.6 11.5L12 20.4z"/>
+    </svg>
+  );
+}
+
+export default BlueHeartIcon;
